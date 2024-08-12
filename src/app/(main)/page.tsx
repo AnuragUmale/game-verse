2c6805b1c757fb506d48ef46536cf1a38fb274fd
Anurag Umale <umale.anurag111@gmail.com>
2024-08-11 17:54:18 -0700

    Chnaged the schema and display the post

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,29 @@
-import Image from "next/image";
+import PostEditor from "@/components/posts/editor/PostEditor";
+import Post from "@/components/posts/Post";
+import prisma from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: {
+      user: {
+        select: {
+          username: true,
+          displayName: true,
+          avatarUrl: true,
+        },
+      },
+    },
+    orderBy: {createdAt: "desc"}
+  })
+
   return (
-    <main>Front Page</main>
+    <main className="flex w-full min-w-0 gap-5">
+      <div className="w-full min-w-0 space-y-5">
+        <PostEditor/>
+        {posts.map(post => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
+    </main>
   );
 }
